75b24bd9561699825927fddd176579d91875edd1
Silvana Camargo <silvanacamargorocha86@gmail.com>
2024-02-23 17:55:43 -0300

    mvp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_02_23_183323) do
-  create_table "restaurants", force: :cascade do |t|
+  create_table 'restaurants', force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.integer "rating"
