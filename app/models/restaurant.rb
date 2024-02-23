98a151b517427b6b165dbfb840ae3893c5febdc4
Silvana Camargo <silvanacamargorocha86@gmail.com>
2024-02-23 15:33:41 -0300

    scaffold restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,2 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
-  validates_presence_of :name, :address, :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,11 @@ puts "Cleaning database..."
 Restaurant.destroy_all
 
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french" }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "french" }
-whatever =  { name: "Whatever", address: "Central Park, London E2 3PO", category: "italian" }
-beijing =  { name: "Beijing", address: "Arsenal Stadium, North-London", category: "chinese" }
-waffle_house =  { name: "Waffle house", address: "Picadilly Circus, Central London", category: "belgian" }
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", rating: 5}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", rating: 4}
 
-[ dishoom, pizza_east, whatever, beijing, waffle_house ].each do |attributes|
+[dishoom, pizza_east].each do |attributes|
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
 end
-
 puts "Finished!"
